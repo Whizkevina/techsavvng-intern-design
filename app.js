a9706c1e0abebd7464d8ec1ef1bcef057313df21
Aina Tim <wizkevinaaina@gmail.com>
2019-04-19 10:03:22 -0400

    changes is inevitable

--- app.js
+++ app.js
@@ -1,9 +1,12 @@
+//import React, { Component, } from 'react';
+//import 'public';
 var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 const ejsLint = require('ejs-lint');
+
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
@@ -28,6 +31,20 @@ app.use('/dropbox', dropboxRouter);
 app.get('/dropbox', function(req, res){
   res.render('/dropbox.ejs');
 });
+
+//React-test
+/* class App extends Component {
+  render() {
+    return (
+    <div className="App">
+      <h2>Welcome to the Road to learn React</h2>
+    </div>
+    );
+  }
+}
+
+export default App;
+*/
 
 //ejsLint(dropboxRouter);
 // catch 404 and forward to error handler
